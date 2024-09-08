3c89579faa6f0f7a4d74ac81aa5c849cc1da3798
ZephyrWolf <ZephyrWolf@users.noreply.github.com>
2024-09-08 21:35:03 +1000

    Fixed bug in WildYams
    Inherited from WorldLitter without consideration for the Waterlogged property

--- src/main/java/io/github/zephyrwolf/medievalism/common/block/WildYamBlock.java
+++ src/main/java/io/github/zephyrwolf/medievalism/common/block/WildYamBlock.java
@@ -1,15 +1,26 @@
 package io.github.zephyrwolf.medievalism.common.block;
 
-import io.github.zephyrwolf.medievalism.content.block.BlockTagCatalog;
+import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SupportType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
 
-public class WildYamBlock extends WorldLitterBlock {
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+public class WildYamBlock extends Block {
 
     public WildYamBlock(Properties properties) {
         super(properties);
@@ -21,7 +32,41 @@ public class WildYamBlock extends WorldLitterBlock {
     }
 
     @Override
+    protected float getShadeBrightness(@NotNull BlockState pState, @NotNull BlockGetter pLevel, @NotNull BlockPos pPos) {
+        return 1.0f;
+    }
+
+    @Override
+    protected boolean propagatesSkylightDown(@NotNull BlockState pState, @NotNull BlockGetter pLevel, @NotNull BlockPos pPos) {
+        return true;
+    }
+
+    @Override // Scheduled Tick from Block Update
+    protected void tick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
+        if (!pState.canSurvive(pLevel, pPos)) {
+            pLevel.destroyBlock(pPos, true);
+        }
+    }
+
+    /**
+     * Update the provided state given the provided neighbor direction and neighbor state, returning a new state.
+     * For example, fences make their connections to the passed in state if possible, and wet concrete powder immediately returns its solidified counterpart.
+     * Note that this method should ideally consider only the specific direction passed in.
+     */
+    // Block Update
+    @Override
+    protected BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, LevelAccessor pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
+        if (!pState.canSurvive(pLevel, pCurrentPos)) {
+            pLevel.scheduleTick(pCurrentPos, this, 1);
+        }
+
+        return super.updateShape(pState, pFacing, pFacingState, pLevel, pCurrentPos, pFacingPos);
+    }
+
+    @Override
     protected boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
-        return pLevel.getBlockState(pPos.below()).is(BlockTags.DIRT) && super.canSurvive(pState, pLevel, pPos);
+        BlockPos belowPos = pPos.below();
+        BlockState belowState = pLevel.getBlockState(belowPos);
+        return belowState.is(BlockTags.DIRT) && belowState.isFaceSturdy(pLevel, belowPos, Direction.UP, SupportType.FULL);
     }
 }
